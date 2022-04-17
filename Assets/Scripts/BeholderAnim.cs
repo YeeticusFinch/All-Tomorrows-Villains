8e002191f950ef2d1e79232bc2fe27bfb2aa5ecf
YeeticusFinch <50182007+YeeticusFinch@users.noreply.github.com>
2022-04-17 00:50:05 -0400

    EYO BEHOLDER ANIMATION POG

---
+++ Assets/Scripts/BeholderAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            anim.Play("Bite");
+        }
+        else if (Input.GetAxisRaw("Horizontal") + Input.GetAxisRaw("Vertical") != 0)
+        {
+            anim.Play("Walk");
+        }
+    }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public int AC = 9;
+    public int HP = 10;
+    public int WALK_SPEED = 30;
+    public int CLIMB_SPEED = 0;
+    public int FLY_SPEED = 0;
+    public bool HOVER = false;
+    public int CR = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
